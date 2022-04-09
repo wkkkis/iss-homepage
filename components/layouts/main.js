123976c3142d067d8196223a3902ebbdb8eeedbc
Issak <aizekyuy@yandex.ru>
2022-04-09 23:24:23 +0600

    some meta fixes

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -20,10 +20,6 @@ const Main = ({ children, router }) => {
         <meta name="author" content="wkkkis" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="/card.png" />
         <meta property="og:site_name" content="Исхак Ишмахаметов" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/card.png" />
