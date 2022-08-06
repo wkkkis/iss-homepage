0138593358e46d0375e9c5c4d7e6ea3040c21d20
Issak <aizekyuy@yandex.ru>
2022-08-06 22:22:05 +0600

    add new fixes

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,41 +1,51 @@
-import Head from 'next/head'
-import dynamic from 'next/dynamic'
-import NavBar from '../navbar'
-import { Box, Container } from '@chakra-ui/react'
-import Footer from '../footer'
-import VoxelDogLoader from '../voxel-dog-loader'
+import Head from "next/head";
+import dynamic from "next/dynamic";
+import NavBar from "../navbar";
+import { Box, Container } from "@chakra-ui/react";
+import Footer from "../footer";
+import VoxelDogLoader from "../voxel-dog-loader";
 
-const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
-  ssr: false,
-  loading: () => <VoxelDogLoader />
-})
+const LazyVoxelDog = dynamic(() => import("../voxel-dog"), {
+    ssr: false,
+    loading: () => <VoxelDogLoader />,
+});
 
 const Main = ({ children, router }) => {
-  return (
-    <Box as="main" pb={8}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="author" content="Исхак Ишмахаметов" />
-        <meta name="description" content="Front-End разработчик" />
-        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta property="og:title" content="Исхак Ишмахаметов" />
-        <meta property="og:image" content="/card.png" />
-        <meta property="og:description" content="Front-End разработчик" />
-        <title>Исхак Ишмахаметов</title>
-      </Head>
+    return (
+        <Box as="main" pb={8}>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <meta name="author" content="Исхак Ишмахаметов" />
+                <meta name="description" content="Front-End разработчик" />
+                <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+                <link
+                    rel="shortcut icon"
+                    href="/favicon.ico"
+                    type="image/x-icon"
+                />
+                <meta property="og:title" content="Исхак Ишмахаметов" />
+                <meta property="og:image" content="/images/issak2.png" />
+                <meta
+                    property="og:description"
+                    content="Front-End разработчик"
+                />
+                <title>Исхак Ишмахаметов</title>
+            </Head>
 
-      <NavBar path={router.asPath} />
+            <NavBar path={router.asPath} />
 
-      <Container maxW="container.md" pt={14}>
-        <LazyVoxelDog />
+            <Container maxW="container.md" pt={14}>
+                <LazyVoxelDog />
 
-        {children}
+                {children}
 
-        <Footer />
-      </Container>
-    </Box>
-  )
-}
+                <Footer />
+            </Container>
+        </Box>
+    );
+};
 
-export default Main
+export default Main;
--- pages/index.js
+++ pages/index.js
@@ -1,172 +1,194 @@
-import NextLink from 'next/link'
+import NextLink from "next/link";
 import {
-  Link,
-  Container,
-  Heading,
-  Box,
-  Image,
-  SimpleGrid,
-  Button,
-  List,
-  ListItem,
-  Icon,
-  useColorModeValue, Text
-} from '@chakra-ui/react'
-import { ChevronRightIcon } from '@chakra-ui/icons'
-import Paragraph from '../components/paragraph'
-import { BioSection, BioYear } from '../components/bio'
-import Layout from '../components/layouts/article'
-import Section from '../components/section'
-import { IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
+    Link,
+    Container,
+    Heading,
+    Box,
+    Image,
+    SimpleGrid,
+    Button,
+    List,
+    ListItem,
+    Icon,
+    useColorModeValue,
+    Text,
+} from "@chakra-ui/react";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import Paragraph from "../components/paragraph";
+import { BioSection, BioYear } from "../components/bio";
+import Layout from "../components/layouts/article";
+import Section from "../components/section";
+import { IoLogoInstagram, IoLogoGithub } from "react-icons/io5";
 
 const Home = () => (
-  <Layout>
-    <Container>
-      <Box
-        borderRadius="lg"
-        mb={6}
-        p={3}
-        textAlign="center"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-      >
-        Привет, Я Front-End разработчик из Кыргызстана!
-      </Box>
+    <Layout>
+        <Container>
+            <Box
+                borderRadius="lg"
+                mb={6}
+                p={3}
+                textAlign="center"
+                bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+            >
+                Привет, Я Front-End разработчик из Кыргызстана!
+            </Box>
 
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading
-            fontFamily="sans-serif"
-            fontSize="30px"
-          >
-            Исхак Ишмахаметов
-          </Heading>
-          <p>Front-End разработчик</p>
-          <a href="/cv/cv.pdf" target='_blank'>
-            <Button mt={3}>
-                ПОСМОТРЕТЬ РЕЗЮМЕ
-            </Button>
-          </a>
-        </Box>
-        <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
-          textAlign="center"
-        >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/images/issak.jpg"
-            alt="Profile image"
-          />
-        </Box>
-      </Box>
+            <Box display={{ md: "flex" }}>
+                <Box flexGrow={1} mb={{ md: 6 }}>
+                    <Heading fontFamily="sans-serif" fontSize="30px">
+                        Исхак Ишмахаметов
+                    </Heading>
+                    <p>
+                        Frontend разработчик с 3 годами опыта во фронтенд
+                        разработке. Основной стек Javascript, Typescript,
+                        Next.js, React.js. Стремлюсь и дальше развиваться в этом
+                        направлении, хочу работать с современными frontend.
+                    </p>
+                    <a href="/cv/cv.pdf" target="_blank">
+                        <Button mt={3}>ПОСМОТРЕТЬ РЕЗЮМЕ</Button>
+                    </a>
+                </Box>
+                <Box
+                    flexShrink={0}
+                    mt={{ base: 4, md: 0 }}
+                    ml={{ md: 6 }}
+                    textAlign="center"
+                >
+                    <Image
+                        borderColor="whiteAlpha.800"
+                        borderWidth={2}
+                        borderStyle="solid"
+                        maxWidth="100px"
+                        display="inline-block"
+                        borderRadius="full"
+                        src="/images/issak2.jpg"
+                        alt="Profile image"
+                    />
+                </Box>
+            </Box>
 
-      <Section delay={0.2}>
-        <Heading
-          as="span"
-          fontFamily="sans-serif"
-          fontSize="20px"
-        >
-          Обо мне
-        </Heading>
-        <BioSection>
-          <BioYear>2019</BioYear>
-          Заинтересовался программированием.
-          Начал активно углубляться в разработку для прокачки своих навыков с помощью фриланс проектов.
-        </BioSection>
-        <BioSection>
-          <BioYear>2020 - 2021</BioYear>
-          Работал в разлиных компаниях улучшая свои навыки в разработке
-        </BioSection>
-        <BioSection>
-          <BioYear>2022</BioYear>
-          Работаю в интересных для меня проектах
-        </BioSection>
-      </Section>
+            <Section delay={0.2}>
+                <Heading as="span" fontFamily="sans-serif" fontSize="20px">
+                    Обо мне
+                </Heading>
+                <BioSection>
+                    <BioYear>2019</BioYear>
+                    Заинтересовался программированием. Начал активно углубляться
+                    в разработку для прокачки своих навыков с помощью фриланс
+                    проектов.
+                </BioSection>
+                <BioSection>
+                    <BioYear>2020 - 2021</BioYear>
+                    Работал в разлиных компаниях улучшая свои навыки в
+                    разработке
+                </BioSection>
+                <BioSection>
+                    <BioYear>2022</BioYear>
+                    Работаю в интересных для меня проектах
+                </BioSection>
+            </Section>
 
-      <Section delay={0.2}>
-        <Heading
-          as="span"
-          fontFamily="sans-serif"
-          fontSize="20px"
-        >
-          Компании
-        </Heading>
-        <BioSection>
-            Ditate.me, Sunrise Studio, Irokez, Makers, Statsnet
-        </BioSection> 
-      </Section>
+            <Section delay={0.2}>
+                <Heading as="span" fontFamily="sans-serif" fontSize="20px">
+                    Компании
+                </Heading>
+                <BioSection>
+                    <BioYear>Июнь 2022 — Август 2022 (3 месяца)</BioYear>
+                    FortyLines
+                </BioSection>
+                <BioSection>
+                    <BioYear>
+                        Октябрь 2021 — По настоящее время (11 месяцев)
+                    </BioYear>
+                    Statsnet
+                </BioSection>
+                <BioSection>
+                    <BioYear>Апрель 2022 — Август 2022 (5 месяца)</BioYear>
+                    Oracle Digital
+                </BioSection>
+                <BioSection>
+                    <BioYear>Июнь 2021 — Декабрь 2021 (7 месяцев)</BioYear>
+                    Makers
+                </BioSection>
+                <BioSection>
+                    <BioYear>Январь 2021 — Июнь 2021 (6 месяцев)</BioYear>
+                    Ditate.me
+                </BioSection>
+                <BioSection>
+                    <BioYear>Сентябрь 2020 — Декабрь 2020 (4 месяца)</BioYear>
+                    Irokez
+                </BioSection>
+                <BioSection>
+                    <BioYear>Август 2020 — Октябрь 2020 (3 месяца)</BioYear>
+                    Sunrise Studio
+                </BioSection>
+                <BioSection>
+                    <BioYear>
+                        Январь 2019 — Апрель 2020 (1 год и 4 месяца)
+                    </BioYear>
+                    Фриланс
+                </BioSection>
+            </Section>
 
-      <Section delay={0.2}>
-        <Heading
-          as="span"
-          fontFamily="sans-serif"
-          fontSize="20px"
-        >
-          Stack
-        </Heading>
-        <BioSection>
-          <BioYear>Web</BioYear>
-          ReactJS, NextJS, NodeJS, TypeScript, Webpack,
-          Axios, Context, Redux, Mobx,
-          JWT, REST API, Redux Thunk, Paddle
-        </BioSection>
-        <BioSection>
-        <BioYear>Web</BioYear>
-          Gulp, Nunjucks, NativeJS, D3JS
-        </BioSection>
-        <BioSection>
-        <BioYear>Web</BioYear>
-          Material UI, TailWind, Styled Components, Bootstrap, CSS Modules, SASS
-        </BioSection>
-        <BioSection>
-        <BioYear>Web</BioYear>
-          HTML, CSS
-        </BioSection>
-      </Section>
+            <Section delay={0.2}>
+                <Heading as="span" fontFamily="sans-serif" fontSize="20px">
+                    Stack
+                </Heading>
+                <BioSection>
+                    <BioYear>Web</BioYear>
+                    ReactJS, NextJS, NodeJS, TypeScript, Webpack, Axios,
+                    Context, Redux, Mobx, JWT, REST API, Redux Thunk, Paddle
+                </BioSection>
+                <BioSection>
+                    <BioYear>Web</BioYear>
+                    Gulp, Nunjucks, NativeJS, D3JS
+                </BioSection>
+                <BioSection>
+                    <BioYear>Web</BioYear>
+                    Material UI, TailWind, Styled Components, Bootstrap, CSS
+                    Modules, SASS
+                </BioSection>
+                <BioSection>
+                    <BioYear>Web</BioYear>
+                    HTML, CSS
+                </BioSection>
+            </Section>
 
-      <Section delay={0.3}>
-        <Heading
-          as="span"
-          fontFamily="sans-serif"
-          fontSize="20px"
-        >
-          Социальные сети
-        </Heading>
-        <List>
-          <ListItem>
-            <Link href="https://github.com/wkkkis" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<Icon as={IoLogoGithub} />}
-              >
-                @wkkkis
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://instagram.com/layryc" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<Icon as={IoLogoInstagram} />}
-              >
-                @layryc
-              </Button>
-            </Link>
-          </ListItem>
-        </List>
+            <Section delay={0.3}>
+                <Heading as="span" fontFamily="sans-serif" fontSize="20px">
+                    Социальные сети
+                </Heading>
+                <List>
+                    <ListItem>
+                        <Link href="https://github.com/wkkkis" target="_blank">
+                            <Button
+                                variant="ghost"
+                                colorScheme="teal"
+                                leftIcon={<Icon as={IoLogoGithub} />}
+                            >
+                                @wkkkis
+                            </Button>
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Link
+                            href="https://instagram.com/layryc"
+                            target="_blank"
+                        >
+                            <Button
+                                variant="ghost"
+                                colorScheme="teal"
+                                leftIcon={<Icon as={IoLogoInstagram} />}
+                            >
+                                @layryc
+                            </Button>
+                        </Link>
+                    </ListItem>
+                </List>
+            </Section>
+        </Container>
+    </Layout>
+);
 
-      </Section>
-    </Container>
-  </Layout>
-)
-
-export default Home
-export { getServerSideProps } from '../components/chakra'
+export default Home;
+export { getServerSideProps } from "../components/chakra";
